b9c600cc3834680ad8591b4920cee5cad8e4d231
Sebastian <mrseb10@gmail.com>
2025-12-28 17:08:42 +0100

    Tablice - Przetwarzanie tablic 1D - Pytanie 1

---
+++ Tablice/Exercises/przetwarzanie_tablic_1d/Pytanie_1.cs
@@ -0,0 +1,23 @@
+﻿namespace Tablice.Exercises.przetwarzanie_tablic_1d;
+
+public class Pytanie_1 {
+    public static void Print(int[] a, int[] b) {
+        string result = "";
+
+        for (var i = 0; i < a.Length; i++) {
+            for (var j = 0; j < b.Length; j++) {
+                if (a[i] == b[j]) 
+                    break;
+                if (j == b.Length - 1)
+                    if (!result.Contains(a[i].ToString())) {
+                        result += $"{a[i]} ";
+                    }
+            }
+        }
+        
+        if (string.IsNullOrEmpty(result)) 
+            Console.WriteLine("empty");
+        else 
+            Console.WriteLine(result.TrimEnd());
+    }
+}
---
+++ Tablice/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tablice.Exercises.przetwarzanie_tablic_1d;
+
+int[] a = new int[] {0, 1, 1, 2, 3, 3, 3};
+int[] b = new int[] {0, 2, 2, 3, 4};
+
+Pytanie_1.Print(a, b);
