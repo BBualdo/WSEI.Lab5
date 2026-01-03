2604031b7d0fe801dad58c36966a1c41bc673237
Sebastian <mrseb10@gmail.com>
2026-01-03 08:25:09 +0100

    Projektowanie Klasy - Czas24H - Pytanie 1

--- ProjektowanieKlasy/Program.cs
+++ ProjektowanieKlasy/Program.cs
@@ -1,4 +1,7 @@
-﻿using ProjektowanieKlasy.Exercises.klasa_person;
+﻿using ProjektowanieKlasy.Exercises.czas_24h;
 
-Person p2 = new Person("Abacka-Kuś", "Ewa", new DateTime(2000, 1, 1));
-Console.WriteLine(p2);
+var t = new Czas24h(2, 15, 37);
+t.Minuta = 20;
+t.Godzina = 1;
+t.Sekunda = 26;
+Console.WriteLine(t);
