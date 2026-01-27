9fdd3aba5ca0279f2514cb536c3dbd138c5b7add
Sebastian <mrseb10@gmail.com>
2026-01-27 17:15:55 +0100

    Interfejsy - Konto w banku - Part 1

---
+++ Interfejsy/Exercises/Bank/Account.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Bank {
+    public class Account : IAccount {
+        private decimal _balance;
+
+        public string Name { get; }
+        public decimal Balance => Math.Round(_balance, 4);
+        public bool IsBlocked { get; private set; }
+
+        public Account(string name, decimal balance = 0) {
+            Name = SetName(name);
+            _balance = SetBalance(balance);
+        }
+    
+        public void Block() {
+            IsBlocked = true;
+        }
+
+        public void Unblock() {
+            IsBlocked = false;
+        }
+
+        public bool Deposit(decimal amount) {
+            if (IsBlocked || amount <= 0) return false;
+            _balance += amount;
+            return true;
+        }
+
+        public bool Withdrawal(decimal amount) {
+            if (IsBlocked || amount <= 0 || amount > Balance) return false;
+            _balance -= amount;
+            return true;
+        }
+
+        public override string ToString() {
+            string result = $"Account name: {Name}, balance: {Balance:F2}";
+            if (IsBlocked) result += ", blocked";
+            return result;
+        }
+
+        private string SetName(string name) {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentOutOfRangeException();
+            name = name.Trim();
+            if (name.Length < 3) throw new ArgumentException();
+            return name;
+        }
+    
+        private decimal SetBalance(decimal balance) {
+            if (balance < 0) throw new ArgumentOutOfRangeException();
+            return balance;
+        }
+    }
+}
---
+++ Interfejsy/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Bank;
+
+var account = new Account(" John   ", 100.23156m);
+Console.WriteLine(account.Balance == 100.2316m);
+Console.WriteLine(account.Name == "John");
+Console.WriteLine(!account.IsBlocked);
+Console.WriteLine(account);
